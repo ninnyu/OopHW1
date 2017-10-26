4a98229a1107a25e08585d7e8c84bb7da0a547e2
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:45:53 -0400

    Update WhereCircles.java

--- WhereCircles.java
+++ WhereCircles.java
@@ -1,13 +1,13 @@
+/*	Homework 1, Question 6
+ *	@author NinnYu Chin
+ *	Instruction: 	Write a program that prompts the user to enter the center coordinates and 
+ *			radiuses of two circles. Determine whether the circles are separate from 
+ *			each other, touching each other, overlapping each other, or completely 
+ *			within one another. Test each case and show the output.
+ */
+
 import java.math.*;
 import java.util.*;
-
-/*	Homework 1, Question 6
-	@author NinnYu Chin
-	Instruction: 	Write a program that prompts the user to enter the center coordinates and 
-			radiuses of two circles. Determine whether the circles are separate from 
-			each other, touching each other, overlapping each other, or completely 
-			within one another. Test each case and show the output.
-*/
 
 public class WhereCircles {
 	public static void main (String[] args) {
