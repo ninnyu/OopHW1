570a69d759747fff4fc91d0fa21108993e9f68d9
NinnYu Chin <nychin92@gmail.com>
2017-10-25 23:15:31 -0400

    Update AreaCircle.java

--- AreaCircle.java
+++ AreaCircle.java
@@ -1,6 +1,6 @@
 /*	Homework 1, Question 2
  *	@author NinnYu Chin
- *	Instruction: 	In a loop, read radiuses from a user. Calculate the area of a circle using 
+ *	Instruction:	In a loop, read radiuses from a user. Calculate the area of a circle using 
  *			the formula PI*r^2. Use the pow function, and the PI constant in the Math 
  *			class. Pick your data types wisely. Exit when the user sends EOF (either 
  *			ctrl-d, or ctrl-z depending on your operating system). Show your output.
