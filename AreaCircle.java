0b6c1c3c7bce7965a9b412ec41b88aee46df59b9
NinnYu Chin <nychin92@gmail.com>
2017-10-25 22:28:16 -0400

    Update AreaCircle.java

--- AreaCircle.java
+++ AreaCircle.java
@@ -1,8 +1,9 @@
 /*	Homework 1, Question 2
 	@author NinnYu Chin
-	Description: 	Reads in the user input, determines if the 
-					input is valid. If valid, calculates the 
-					area of a circle using the input as the radius.
+	Instruction: 	In a loop, read radiuses from a user. Calculate the area of a circle using 
+			the formula PI*r^2. Use the pow function, and the PI constant in the Math 
+			class. Pick your data types wisely. Exit when the user sends EOF (either 
+			ctrl-d, or ctrl-z depending on your operating system). Show your output.
 */
 
 import java.util.*;
